e626628b194f457de22e5b495e833d9aba78c0eb
skepticus <dcbrantley@hotmail.com>
2019-01-21 09:18:17 -0500

    Initial commit

---
+++ Source/ProvingGround/ProvingGroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProvingGroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProvingGroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProvingGroundGameMode();
+};
+
+
+
